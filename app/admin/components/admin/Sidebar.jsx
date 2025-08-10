244d47b720426e565b0e111b26de65f93ba382ec
Ajeet chaudhary <ajeettharu0@gmail.com>
2025-08-10 11:58:15 +0530

    updated

--- app/admin/components/admin/Sidebar.jsx
+++ app/admin/components/admin/Sidebar.jsx
@@ -1,16 +1,18 @@
 "use client";
 import { useState } from "react";
-import { FaBars, FaTachometerAlt, FaUser, FaCog } from "react-icons/fa";
+import { FaBars, FaTachometerAlt, FaUser, FaCog, FaSignOutAlt } from "react-icons/fa";
 import Link from "next/link";
+import { signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 export default function Sidebar() {
+  const { data: session, status } = useSession();
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   return (
     <div
-      className={`h-screen bg-gray-800 text-white transition-all duration-300 ${
-        isCollapsed ? "w-16" : "w-64"
-      }`}
+      className={`h-screen bg-gray-800 text-white transition-all duration-300 ${isCollapsed ? "w-16" : "w-64"
+        }`}
     >
       {/* Toggle Button */}
       <div className="flex justify-end p-2">
@@ -20,19 +22,35 @@ export default function Sidebar() {
       </div>
 
       {/* Menu Items */}
-      <nav className="mt-4">
-        <Link href="/admin/dashboard" className="flex items-center p-3 hover:bg-gray-700">
-          <FaTachometerAlt className="mr-2" />
-          {!isCollapsed && "Dashboard"}
-        </Link>
-        <Link href="/admin/users" className="flex items-center p-3 hover:bg-gray-700">
-          <FaUser className="mr-2" />
-          {!isCollapsed && "Users"}
-        </Link>
-        <Link href="/admin/settings" className="flex items-center p-3 hover:bg-gray-700">
-          <FaCog className="mr-2" />
-          {!isCollapsed && "Settings"}
-        </Link>
+      <nav className="mt-4 flex flex-col justify-between h-[calc(100%-3rem)]">
+        <div>
+          <Link href="/admin/dashboard" className="flex items-center p-3 hover:bg-gray-700">
+            <FaTachometerAlt className="mr-2" />
+            {!isCollapsed && "Dashboard"}
+          </Link>
+          <Link href="/admin/users" className="flex items-center p-3 hover:bg-gray-700">
+            <FaUser className="mr-2" />
+            {!isCollapsed && "Users"}
+          </Link>
+          <Link href="/admin/settings" className="flex items-center p-3 hover:bg-gray-700">
+            <FaCog className="mr-2" />
+            {!isCollapsed && "Settings"}
+          </Link>
+        </div>
+
+        {/* Logout */}
+        {
+          session?.user?.name && (
+            <button
+              onClick={() => signOut({ callbackUrl: "/login" })}
+              className="flex items-center p-3 hover:bg-gray-700"
+            >
+              <FaSignOutAlt className="mr-2" />
+              {!isCollapsed && "Logout"}
+            </button>
+          )
+        }
+
       </nav>
     </div>
   );
